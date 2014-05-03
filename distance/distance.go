1ac014776152e84c32c0a8855e63d8b680ede358
Alex Petrov <alexp@coffeenco.de>
2014-05-03 23:32:53 +0200

    Add Cranberra distance measure

--- distance/distance.go
+++ distance/distance.go
@@ -4,10 +4,29 @@ import (
 	"math"
 )
 
-func eucledianDistance (p1 []float64, p2 []float64) float64 {
+func EucledianDistance(p1 []float64, p2 []float64) float64 {
 	sum := float64(0)
 	for i := 0; i < cap(p1); i++ {
 		sum += math.Pow(p1[i] - p2[i], 2)
 	}
 	return math.Sqrt(sum);
+}
+
+func cranberraDistanceStep(num float64, denom float64) float64 {
+	if num == float64(0) && denom == float64(0) {
+		return float64(0)
+	} else {
+		return num/denom
+	}
+}
+
+func CranberraDistance(p1 []float64, p2 []float64) float64 {
+	sum := float64(0)
+
+	for i := 0; i < cap(p1); i++ {
+		num := math.Abs(p1[i] - p2[i])
+		denom := math.Abs(p1[i]) + math.Abs(p2[i])
+		sum += cranberraDistanceStep(num, denom)
+	}
+	return sum;
 }
--- distance/distance_test.go
+++ distance/distance_test.go
@@ -9,10 +9,35 @@ import (
 func TestEucledianDistance(t *testing.T) {
 
 	Convey("Given two vectors", t, func() {
-		distance := eucledianDistance([]float64 { 1, -2, 3, 4 }, []float64 { -5, -6, 7, 8 })
+		distance := EucledianDistance([]float64 { 1, -2, 3, 4 }, []float64 { -5, -6, 7, 8 })
 
 		Convey("The result should be a square root of 84", func() {
 			So(distance, ShouldEqual, math.Sqrt(84))
+		})
+
+	})
+
+}
+
+func TestCranberraDistance(t *testing.T) {
+
+	Convey("Given two vectors that are same", t, func() {
+		vec := []float64 { 0, 1, -2, 3.4, 5, -6.7, 89 }
+		distance := CranberraDistance(vec, vec)
+
+		Convey("The result should be 0", func() {
+			So(distance, ShouldEqual, 0)
+		})
+
+	})
+
+	Convey("Given two vectors", t, func() {
+		p1 := []float64 { 1, 2, 3, 4, 9 }
+		p2 := []float64 { -5, -6, 7, 4, 3 };
+		distance := CranberraDistance(p1, p2)
+
+		Convey("The result should be 0", func() {
+			So(distance, ShouldEqual, 2.9)
 		})
 
 	})
