83faf52273865c8342f7f8d7e21a9609849ad819
Bert Chang <jrweizhang@gmail.com>
2014-05-03 15:29:07 +0800

    Remove examples, remove CheckDimMatch.

--- metrics/pairwise/pairwise.go
+++ metrics/pairwise/pairwise.go
@@ -1,13 +1,1 @@
 package pairwise
-
-import (
-	mat "github.com/skelterjohn/go.matrix"
-)
-
-func CheckDimMatch(vectorX *mat.DenseMatrix, vectorY *mat.DenseMatrix) {
-	if vectorX.Cols() != 1 ||
-		vectorY.Cols() != 1 ||
-		vectorX.Rows() != vectorY.Rows() {
-		panic("Dimension mismatch")
-	}
-}
