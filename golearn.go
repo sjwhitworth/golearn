a92d830ec353306fa5c5273e114612c2f3446b06
Stephen Whitworth <stephenjameswhitworth@gmail.com>
2014-05-04 09:38:35 +0100

    Update golearn.go

--- golearn.go
+++ golearn.go
@@ -1,2 +1,2 @@
-//golearn is a machine learning library for Go. It provides a host of algorithms, with lots of helper functions to make life easier.
+//golearn is a machine learning library for Go.
 package golearn
