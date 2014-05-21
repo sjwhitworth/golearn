86b18fe1c963f8afb8d4b88c2ed3199f48e84fdc
Thiago Cardoso <thiagoncc@gmail.com>
2014-05-20 21:41:57 -0300

    Finished Fit test for simple cases
    Added the logCondProb tests for a simple test case with two classes.

--- naive/bernoulli_nb_test.go
+++ naive/bernoulli_nb_test.go
@@ -26,8 +26,23 @@ func TestFit(t *testing.T) {
         })
 
         Convey("'red' conditional probabilities should be correct", func() {
+            logCondProbTok0 := nb.logCondProb["red"][0]
+            logCondProbTok1 := nb.logCondProb["red"][1]
+            logCondProbTok2 := nb.logCondProb["red"][2]
+
+            So(logCondProbTok0, ShouldAlmostEqual, math.Log(1.0))
+            So(logCondProbTok1, ShouldAlmostEqual, math.Log(1.0/3.0))
+            So(logCondProbTok2, ShouldAlmostEqual, math.Log(1.0))
         })
+
         Convey("'blue' conditional probabilities should be correct", func() {
+            logCondProbTok0 := nb.logCondProb["blue"][0]
+            logCondProbTok1 := nb.logCondProb["blue"][1]
+            logCondProbTok2 := nb.logCondProb["blue"][2]
+
+            So(logCondProbTok0, ShouldAlmostEqual, math.Log(1.0))
+            So(logCondProbTok1, ShouldAlmostEqual, math.Log(1.0))
+            So(logCondProbTok2, ShouldAlmostEqual, math.Log(1.0/3.0))
         })
     })
 }
