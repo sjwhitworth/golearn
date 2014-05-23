ce2afe34fb2975ccd060fe67cace660964802ad9
Richard Townsend <richard@sentimentron.co.uk>
2014-05-23 11:56:23 +0100

    Benchmarking

--- meta/bagging_test.go
+++ meta/bagging_test.go
@@ -11,6 +11,49 @@ import (
 	"time"
 )
 
+func BenchmarkBaggingRandomForestFit(testEnv *testing.B) {
+	inst, err := base.ParseCSVToInstances("../examples/datasets/iris_headers.csv", true)
+	if err != nil {
+		panic(err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	filt := filters.NewChiMergeFilter(inst, 0.90)
+	filt.AddAllNumericAttributes()
+	filt.Build()
+	filt.Run(inst)
+	rf := new(BaggedModel)
+	for i := 0; i < 10; i++ {
+		rf.AddModel(trees.NewRandomTree(2))
+	}
+	testEnv.ResetTimer()
+	for i := 0; i < 20; i++ {
+		rf.Fit(inst)
+	}
+}
+
+func BenchmarkBaggingRandomForestPredict(testEnv *testing.B) {
+	inst, err := base.ParseCSVToInstances("../examples/datasets/iris_headers.csv", true)
+	if err != nil {
+		panic(err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	filt := filters.NewChiMergeFilter(inst, 0.90)
+	filt.AddAllNumericAttributes()
+	filt.Build()
+	filt.Run(inst)
+	rf := new(BaggedModel)
+	for i := 0; i < 10; i++ {
+		rf.AddModel(trees.NewRandomTree(2))
+	}
+	rf.Fit(inst)
+	testEnv.ResetTimer()
+	for i := 0; i < 20; i++ {
+		rf.Predict(inst)
+	}
+}
+
 func TestRandomForest1(testEnv *testing.T) {
 	inst, err := base.ParseCSVToInstances("../examples/datasets/iris_headers.csv", true)
 	if err != nil {
