c2d040af3097390472111c9dddf25424d8a5604e
Richard Townsend <richard@sentimentron.co.uk>
2014-08-02 16:22:15 +0100

    trees: merge from v2-instances

--- trees/tree_test.go
+++ trees/tree_test.go
@@ -14,15 +14,17 @@ func TestRandomTree(testEnv *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-
 	filt := filters.NewChiMergeFilter(inst, 0.90)
-	filt.AddAllNumericAttributes()
-	filt.Build()
-	filt.Run(inst)
-	fmt.Println(inst)
+	for _, a := range base.NonClassFloatAttributes(inst) {
+		filt.AddAttribute(a)
+	}
+	filt.Train()
+	instf := base.NewLazilyFilteredInstances(inst, filt)
+
 	r := new(RandomTreeRuleGenerator)
 	r.Attributes = 2
-	root := InferID3Tree(inst, r)
+	fmt.Println(instf)
+	root := InferID3Tree(instf, r)
 	fmt.Println(root)
 }
 
@@ -33,18 +35,20 @@ func TestRandomTreeClassification(testEnv *testing.T) {
 	}
 	trainData, testData := base.InstancesTrainTestSplit(inst, 0.6)
 	filt := filters.NewChiMergeFilter(inst, 0.90)
-	filt.AddAllNumericAttributes()
-	filt.Build()
-	filt.Run(trainData)
-	filt.Run(testData)
-	fmt.Println(inst)
+	for _, a := range base.NonClassFloatAttributes(inst) {
+		filt.AddAttribute(a)
+	}
+	filt.Train()
+	trainDataF := base.NewLazilyFilteredInstances(trainData, filt)
+	testDataF := base.NewLazilyFilteredInstances(testData, filt)
+
 	r := new(RandomTreeRuleGenerator)
 	r.Attributes = 2
-	root := InferID3Tree(trainData, r)
+	root := InferID3Tree(trainDataF, r)
 	fmt.Println(root)
-	predictions := root.Predict(testData)
+	predictions := root.Predict(testDataF)
 	fmt.Println(predictions)
-	confusionMat := eval.GetConfusionMatrix(testData, predictions)
+	confusionMat := eval.GetConfusionMatrix(testDataF, predictions)
 	fmt.Println(confusionMat)
 	fmt.Println(eval.GetMacroPrecision(confusionMat))
 	fmt.Println(eval.GetMacroRecall(confusionMat))
@@ -58,17 +62,19 @@ func TestRandomTreeClassification2(testEnv *testing.T) {
 	}
 	trainData, testData := base.InstancesTrainTestSplit(inst, 0.4)
 	filt := filters.NewChiMergeFilter(inst, 0.90)
-	filt.AddAllNumericAttributes()
-	filt.Build()
-	fmt.Println(testData)
-	filt.Run(testData)
-	filt.Run(trainData)
+	for _, a := range base.NonClassFloatAttributes(inst) {
+		filt.AddAttribute(a)
+	}
+	filt.Train()
+	trainDataF := base.NewLazilyFilteredInstances(trainData, filt)
+	testDataF := base.NewLazilyFilteredInstances(testData, filt)
+
 	root := NewRandomTree(2)
-	root.Fit(trainData)
+	root.Fit(trainDataF)
 	fmt.Println(root)
-	predictions := root.Predict(testData)
+	predictions := root.Predict(testDataF)
 	fmt.Println(predictions)
-	confusionMat := eval.GetConfusionMatrix(testData, predictions)
+	confusionMat := eval.GetConfusionMatrix(testDataF, predictions)
 	fmt.Println(confusionMat)
 	fmt.Println(eval.GetMacroPrecision(confusionMat))
 	fmt.Println(eval.GetMacroRecall(confusionMat))
@@ -82,19 +88,21 @@ func TestPruning(testEnv *testing.T) {
 	}
 	trainData, testData := base.InstancesTrainTestSplit(inst, 0.6)
 	filt := filters.NewChiMergeFilter(inst, 0.90)
-	filt.AddAllNumericAttributes()
-	filt.Build()
-	fmt.Println(testData)
-	filt.Run(testData)
-	filt.Run(trainData)
+	for _, a := range base.NonClassFloatAttributes(inst) {
+		filt.AddAttribute(a)
+	}
+	filt.Train()
+	trainDataF := base.NewLazilyFilteredInstances(trainData, filt)
+	testDataF := base.NewLazilyFilteredInstances(testData, filt)
+
 	root := NewRandomTree(2)
-	fittrainData, fittestData := base.InstancesTrainTestSplit(trainData, 0.6)
+	fittrainData, fittestData := base.InstancesTrainTestSplit(trainDataF, 0.6)
 	root.Fit(fittrainData)
 	root.Prune(fittestData)
 	fmt.Println(root)
-	predictions := root.Predict(testData)
+	predictions := root.Predict(testDataF)
 	fmt.Println(predictions)
-	confusionMat := eval.GetConfusionMatrix(testData, predictions)
+	confusionMat := eval.GetConfusionMatrix(testDataF, predictions)
 	fmt.Println(confusionMat)
 	fmt.Println(eval.GetMacroPrecision(confusionMat))
 	fmt.Println(eval.GetMacroRecall(confusionMat))
@@ -142,6 +150,7 @@ func TestID3Inference(testEnv *testing.T) {
 		testEnv.Error(sunnyChild)
 	}
 	if rainyChild.SplitAttr.GetName() != "windy" {
+		fmt.Println(rainyChild.SplitAttr)
 		testEnv.Error(rainyChild)
 	}
 	if overcastChild.SplitAttr != nil {
@@ -156,7 +165,6 @@ func TestID3Inference(testEnv *testing.T) {
 	if sunnyLeafNormal.Class != "yes" {
 		testEnv.Error(sunnyLeafNormal)
 	}
-
 	windyLeafFalse := rainyChild.Children["false"]
 	windyLeafTrue := rainyChild.Children["true"]
 	if windyLeafFalse.Class != "yes" {
@@ -176,12 +184,18 @@ func TestID3Classification(testEnv *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	filt := filters.NewBinningFilter(inst, 10)
-	filt.AddAllNumericAttributes()
-	filt.Build()
-	filt.Run(inst)
 	fmt.Println(inst)
-	trainData, testData := base.InstancesTrainTestSplit(inst, 0.70)
+	filt := filters.NewBinningFilter(inst, 10)
+	for _, a := range base.NonClassFloatAttributes(inst) {
+		filt.AddAttribute(a)
+	}
+	filt.Train()
+	fmt.Println(filt)
+	instf := base.NewLazilyFilteredInstances(inst, filt)
+	fmt.Println("INSTFA", instf.AllAttributes())
+	fmt.Println("INSTF", instf)
+	trainData, testData := base.InstancesTrainTestSplit(instf, 0.70)
+
 	// Build the decision tree
 	rule := new(InformationGainRuleGenerator)
 	root := InferID3Tree(trainData, rule)
@@ -199,6 +213,7 @@ func TestID3(testEnv *testing.T) {
 
 	// Import the "PlayTennis" dataset
 	inst, err := base.ParseCSVToInstances("../examples/datasets/tennis.csv", true)
+	fmt.Println(inst)
 	if err != nil {
 		panic(err)
 	}
