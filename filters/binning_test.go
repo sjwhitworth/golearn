cb9a6bcf767df29f923736706fd81cd0f7fc6d36
Richard Townsend <richard@sentimentron.co.uk>
2014-08-03 16:58:08 +0100

    filters: revised binning_test to be more robust

--- filters/binning_test.go
+++ filters/binning_test.go
@@ -6,35 +6,51 @@ import (
 	"testing"
 )
 
-func TestBinning(testEnv *testing.T) {
-	//
-	// Read the data
-	inst1, err := base.ParseCSVToInstances("../examples/datasets/iris_headers.csv", true)
-	if err != nil {
-		panic(err)
-	}
-
-	inst2, err := base.ParseCSVToInstances("../examples/datasets/iris_binned.csv", true)
-	if err != nil {
-		panic(err)
-	}
-	//
-	// Construct the binning filter
-	binAttr := inst1.AllAttributes()[0]
-	filt := NewBinningFilter(inst1, 10)
-	filt.AddAttribute(binAttr)
-	filt.Train()
-	inst1f := base.NewLazilyFilteredInstances(inst1, filt)
-
-	// Retrieve the categorical version of the original Attribute
-
-	//
-	// Create the LazilyFilteredInstances
-	// and check the values
-	Convey("Discretized version should match reference", testEnv, func() {
-		_, rows := inst1.Size()
-		for i := 0; i < rows; i++ {
-			So(inst1f.RowString(i), ShouldEqual, inst2.RowString(i))
+func TestBinning(t *testing.T) {
+	Convey("Given some data and a reference", t, func() {
+		// Read the data
+		inst1, err := base.ParseCSVToInstances("../examples/datasets/iris_headers.csv", true)
+		if err != nil {
+			panic(err)
 		}
+
+		inst2, err := base.ParseCSVToInstances("../examples/datasets/iris_binned.csv", true)
+		if err != nil {
+			panic(err)
+		}
+		//
+		// Construct the binning filter
+		binAttr := inst1.AllAttributes()[0]
+		filt := NewBinningFilter(inst1, 10)
+		filt.AddAttribute(binAttr)
+		filt.Train()
+		inst1f := base.NewLazilyFilteredInstances(inst1, filt)
+
+		// Retrieve the categorical version of the original Attribute
+		var cAttr base.Attribute
+		for _, a := range inst1f.AllAttributes() {
+			if a.GetName() == binAttr.GetName() {
+				cAttr = a
+			}
+		}
+
+		cAttrSpec, err := inst1f.GetAttribute(cAttr)
+		So(err, ShouldEqual, nil)
+		binAttrSpec, err := inst2.GetAttribute(binAttr)
+		So(err, ShouldEqual, nil)
+
+		//
+		// Create the LazilyFilteredInstances
+		// and check the values
+		Convey("Discretized version should match reference", func() {
+			_, rows := inst1.Size()
+			for i := 0; i < rows; i++ {
+				val1 := inst1f.Get(cAttrSpec, i)
+				val2 := inst2.Get(binAttrSpec, i)
+				val1s := cAttr.GetStringFromSysVal(val1)
+				val2s := binAttr.GetStringFromSysVal(val2)
+				So(val1s, ShouldEqual, val2s)
+			}
+		})
 	})
 }
