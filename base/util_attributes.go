9aa4ee64b5000c5af731e1f9b30e456398b86029
Linker Lin <linker.lin@live.com>
2017-02-14 15:08:12 +0800

    Update util_attributes.go
    replaced range by []

--- base/util_attributes.go
+++ base/util_attributes.go
@@ -42,7 +42,9 @@ func NonClassAttributes(d DataGrid) []Attribute {
 // all of the Attributes.
 func ResolveAttributes(d DataGrid, attrs []Attribute) []AttributeSpec {
 	ret := make([]AttributeSpec, len(attrs))
-	for i, a := range attrs {
+        n := len(attrs)
+        for i := 0; i < n; i++ {
+                a := attrs[i]
 		spec, err := d.GetAttribute(a)
 		if err != nil {
 			panic(fmt.Errorf("Error resolving Attribute %s: %s", a, err))
